e5aa04ee544e020261616d887059434298c96eab
Michael Muther <michaelmuther@yahoo.com>
2022-02-05 22:16:53 +0100

    Stage 2: completed

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,31 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
-    final static String INPUT_ENTER_CELLS_STR = "Enter the cells:";
     final static String INPUT_ENTER_COORDINATES_STR = "Enter the coordinates:";
     final static String ERROR_OCCUPIED_CELL_STR = "This cell is occupied! Choose another one!";
     final static String ERROR_INVALID_ENTRY_STR = "You should enter numbers!";
     final static String ERROR_INPUT_OUT_OF_RANGE_STR = "Coordinates should be from 1 to 3!";
     final static String STATE_DRAW_STR = "Draw";
-    final static String STATE_X_WINS_STR = "X wins";
-    final static String STATE_O_WINS_STR = "O wins";
     final static String STATE_WIN_STR = " wins";
     final static String STATE_GAME_NOT_FINISHED_STR = "Game not finished";
+    final static String MOVE_GAME_LEVEL_EASY_STR = "Making move level \"easy\"";
     final static String GAME_GRID_BORDER_STR = "---------";
     final static String GAME_GRID_ROW_STR = "| %C %C %C |\n";
     final static char X_CHAR = 'X';
     final static char O_CHAR = 'O';
+    final static char SPACE_CHAR = ' ';
     static Scanner scanner = new Scanner(System.in);
+    static Random rand = new Random(System.currentTimeMillis());
+    static boolean gameOver = false;
+    static char[][] gameGrid = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+    };
 
-    static char[][] gameGrid = new char[3][3];
-
-    static String input = "";
-
-    static boolean xTurn = true;
-    static char turn = 'X';
-
-    public static char[][] stringToTwoDimensionalArray(String input) {
-        char[][] temp = new char[3][3];
-        int arrayRows = 3;
-        int arrayColumns = 3;
-        int inputIndex = 0;
-        for (int i = 0; i < arrayRows; i++) {
-            for (int j = 0; j < arrayColumns; j++) {
-                if (input.charAt(inputIndex) != '_') {
-                    temp[i][j] = input.charAt(inputIndex);
-                } else {
-                    temp[i][j] = ' ';
-                }
-                inputIndex++;
-            }
-        }
-        return temp;
-    }
+    static char turn = X_CHAR;
 
     public static void printGameGrid(char[][] gameGrid) {
         System.out.println(GAME_GRID_BORDER_STR);
@@ -49,11 +33,6 @@ public class Main {
         System.out.printf(GAME_GRID_ROW_STR, gameGrid[1][0], gameGrid[1][1], gameGrid[1][2]);
         System.out.printf(GAME_GRID_ROW_STR, gameGrid[2][0], gameGrid[2][1], gameGrid[2][2]);
         System.out.println(GAME_GRID_BORDER_STR);
-    }
-
-    public static void gameInputString() {
-        System.out.println(INPUT_ENTER_CELLS_STR);
-        input = scanner.nextLine();
     }
 
     public static boolean isNumeric(String str) {
@@ -68,23 +47,12 @@ public class Main {
     public static boolean checkCellInput(int[] cellIndices, char[][] gameGrid) {
         int index1 = cellIndices[0];
         int index2 = cellIndices[1];
-        return gameGrid[index1][index2] == 'X' || gameGrid[index1][index2] == 'O';
+        return gameGrid[index1][index2] == X_CHAR || gameGrid[index1][index2] == O_CHAR;
     }
 
     public static boolean checkGameOver(char[][] gameGrid, char xo) {
         String winMessage = xo + STATE_WIN_STR;
         int turns = 0;
-//        for (int i = 0; i < gameGrid.length; i++) {
-//            for (int j = 0; j < gameGrid[i].length; j++) {
-//                if (gameGrid[i][j] != ' ') {
-//                    turns++;
-//                }
-//            }
-//        }
-//        if (turns == 9) {
-//            System.out.println(STATE_DRAW_STR);
-//            return true;
-//        }
         boolean win = false;
         if (gameGrid[0][0] == xo && gameGrid[0][1] == xo && gameGrid[0][2] == xo) { //row 1 horizontal
             win = true;
@@ -117,8 +85,6 @@ public class Main {
         if (turns == 9) {
             System.out.println(STATE_DRAW_STR);
             return true;
-        } else {
-            System.out.println(STATE_GAME_NOT_FINISHED_STR);
         }
         return win;
     }
@@ -135,17 +101,7 @@ public class Main {
         return count;
     }
 
-    public static char determineFirstTurn(char[][] gameGrid) {
-        int xCount = countXo(gameGrid, X_CHAR);
-        int oCount = countXo(gameGrid, O_CHAR);
-        return xCount > oCount ? O_CHAR : X_CHAR;
-    }
-
-    public static void main(String[] args) {
-        gameInputString();
-        gameGrid = stringToTwoDimensionalArray(input);
-        turn = determineFirstTurn(gameGrid);
-        printGameGrid(gameGrid);
+    public static void userTurn() {
         boolean validCoordinateInput = false;
         int one = 1; // difference between coordinate and index
         int[] cellIndices = new int[2];
@@ -174,14 +130,104 @@ public class Main {
                     System.out.println(ERROR_OCCUPIED_CELL_STR);
                 } else {
                     validCoordinateInput = true;
-                    gameGrid[cellIndices[0]][cellIndices[1]] = turn;
-                    xTurn = !xTurn;
+                    gameGrid[cellIndices[0]][cellIndices[1]] = X_CHAR;
                 }
             } else {
                 System.out.println(ERROR_INPUT_OUT_OF_RANGE_STR);
             }
         } while (!validCoordinateInput);
         printGameGrid(gameGrid);
-        checkGameOver(gameGrid,turn);
+        gameOver = checkGameOver(gameGrid, turn);
+        turn = O_CHAR;
     }
+
+    public static void computerTurn() {
+        boolean validMove = false;
+        int randRow;
+        int randCol;
+        do {
+            randRow = rand.nextInt(3);
+            randCol = rand.nextInt(3);
+            if (gameGrid[randRow][randCol] == SPACE_CHAR) {
+                gameGrid[randRow][randCol] = O_CHAR;
+                validMove = true;
+            }
+        } while (!validMove);
+
+        System.out.println(MOVE_GAME_LEVEL_EASY_STR);
+        printGameGrid(gameGrid);
+        gameOver = checkGameOver(gameGrid, turn);
+        turn = X_CHAR;
+    }
+
+    public static void playGame() {
+        printGameGrid(gameGrid);
+        do {
+            if (turn == X_CHAR) {
+                userTurn();
+            } else {
+                computerTurn();
+            }
+        } while (!gameOver);
+    }
+
+    public static void main(String[] args) {
+        playGame();
+    }
+
 }
+
+//        for (int i = 0; i < gameGrid.length; i++) {
+//            for (int j = 0; j < gameGrid[i].length; j++) {
+//                if (gameGrid[i][j] != ' ') {
+//                    turns++;
+//                }
+//            }
+//        }
+//        if (turns == 9) {
+//            System.out.println(STATE_DRAW_STR);
+//            return true;
+//        }
+
+//    final static String STATE_X_WINS_STR = "X wins";
+//    final static String STATE_O_WINS_STR = "O wins";
+
+//    public static char[][] stringToTwoDimensionalArray(String input) {
+//        char[][] temp = new char[3][3];
+//        int arrayRows = 3;
+//        int arrayColumns = 3;
+//        int inputIndex = 0;
+//        for (int i = 0; i < arrayRows; i++) {
+//            for (int j = 0; j < arrayColumns; j++) {
+//                if (input.charAt(inputIndex) != '_') {
+//                    temp[i][j] = input.charAt(inputIndex);
+//                } else {
+//                    temp[i][j] = ' ';
+//                }
+//                inputIndex++;
+//            }
+//        }
+//        return temp;
+//    }
+
+//    public static void gameInputString() {
+//        System.out.println(INPUT_ENTER_CELLS_STR);
+//        input = scanner.nextLine();
+//    }
+
+
+
+//    public static char determineFirstTurn(char[][] gameGrid) {
+//        int xCount = countXo(gameGrid, X_CHAR);
+//        int oCount = countXo(gameGrid, O_CHAR);
+//        return xCount > oCount ? O_CHAR : X_CHAR;
+//    }
+
+//        gameGrid = stringToTwoDimensionalArray(input);
+//        turn = determineFirstTurn(gameGrid);
+//    gameInputString();
+//    static char[][] gameGrid = {new char[3][3]};
+
+//    final static String INPUT_ENTER_CELLS_STR = "Enter the cells:";
+
+//    static String input = "";
